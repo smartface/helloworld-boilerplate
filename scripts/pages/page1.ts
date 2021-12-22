a5a8d482b0fc575a1fdf51a4827fd681bba0ff1f
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2021-12-22 14:14:20 +0300

    general improvements

--- scripts/pages/page1.ts
+++ scripts/pages/page1.ts
@@ -1,18 +1,16 @@
 import Page1Design from 'generated/pages/page1';
-import componentContextPatch from '@smartface/contx/lib/smartface/componentContextPatch';
 import PageTitleLayout from 'components/PageTitleLayout';
 import System from '@smartface/native/device/system';
+import Label from '@smartface/native/ui/label';
 
 export default class Page1 extends Page1Design {
     constructor(private router: any) {
         super();
-        // Overrides super.onShow method
-        this.onShow = onShow.bind(this, this.onShow.bind(this));
-        // Overrides super.onLoad method
-        this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
         this.btnNext.onPress = () => {
             this.router.push('/pages/page2', { message: 'Hello World!' });
         };
+
+        console.log("Page1 : ");
     }
 
 
@@ -20,35 +18,34 @@ export default class Page1 extends Page1Design {
      * @event onShow
      * This event is called when a page appears on the screen (everytime).
      */
-     onShow() {
-		super.onShow();
+    onShow() {
+        super.onShow();
         console.log("onShow Page1");
         // this.headerBar.titleLayout.applyLayout();
         const lbl = new Label();
         this.addChild(lbl, 'page1lbl1unique');
-        this.addStyleableChild(lbl, "page1lbl1unique", 'sf-label',  (userProps) => {
-            userProps.height = 50;
-            return userProps;
+        this.addStyleableChild(lbl, "page1lbl1unique", 'sf-label', (userProps: Record<string, any>) => {
+            return {...userProps, height: 50};
         });
 
         lbl.text = "It's a runtime label";
 
         this.headerBar.titleLayout.applyLayout();
-	}
+    }
 
-	/**
-	 * @event onLoad
-	 * This event is called once when page is created.
-	 */
-	onLoad() {
-		super.onLoad?.();
-        // console.log("Onload Page1 : ", super.onLoad);
+    /**
+     * @event onLoad
+     * This event is called once when page is created.
+     */
+    onLoad() {
+        super.onLoad();
+        console.log("Onload Page1 : ", super.onLoad);
         this.headerBar.leftItemEnabled = false;
         this.headerBar.titleLayout = new PageTitleLayout();
         this.addStyleableChild(this.headerBar.titleLayout, "titleLayout");
         if (System.OS === 'Android') {
             this.headerBar.title = '';
         }
-	}
+    }
 
 }
--- scripts/pages/page2.ts
+++ scripts/pages/page2.ts
@@ -3,64 +3,62 @@ import HeaderBarItem from '@smartface/native/ui/headerbaritem';
 import touch from '@smartface/extension-utils/lib/touch';
 import Image from '@smartface/native/ui/image';
 import PageTitleLayout from 'components/PageTitleLayout';
-import componentContextPatch from '@smartface/contx/lib/smartface/componentContextPatch';
 import Color from '@smartface/native/ui/color';
 import System from '@smartface/native/device/system';
+import HeaderBar from '@smartface/native/ui/headerbar';
+import View from '@smartface/native/ui/view';
 
 export default class Page2 extends Page2Design {
-  routeData: any;
-  parentController: any;
-  constructor(private router: any) {
-    super();
-    // Overrides super.onShow method
-    this.onShow = onShow.bind(this, this.onShow.bind(this));
-    // Overrides super.onLoad method
-    this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
-    touch.addPressEvent(this.btnSayHello, () => {
-      alert('Hello World!');
-    });
-  }
-}
+    routeData: any;
+    parentController: any;
 
-/**
- * @event onShow
- * This event is called when a page appears on the screen (everytime).
- */
-function onShow(this: Page2, superOnShow: () => void) {
-  superOnShow();
-  this.headerBar.titleLayout.applyLayout();
-  this.routeData && console.info(this.routeData.message);
-}
+    constructor(private router: any) {
+        super();
+        touch.addPressEvent(this.btnSayHello as View<any>, () => {
+            alert('Hello World!');
+        });
+    }
 
-/**
- * @event onLoad
- * This event is called once when page is created.
- */
-function onLoad(this: Page2, superOnLoad: () => void) {
-  superOnLoad();
-  let headerBar;
-  this.headerBar.titleLayout = new PageTitleLayout();
-  componentContextPatch(this.headerBar.titleLayout, 'titleLayout');
-  this.headerBar.setItems([
-    new HeaderBarItem({
-      title: 'Option',
-      onPress: () => {
-        console.warn('You pressed Option item!');
-      },
-    }),
-  ]);
-  if (System.OS === 'Android') {
-    headerBar = this.headerBar;
-    headerBar.setLeftItem(
-      new HeaderBarItem({
-        onPress: () => {
-          this.router.goBack();
-        },
-        image: Image.createFromFile('images://arrow_back.png'),
-      }),
-    );
-  } else {
-    headerBar = this.parentController.headerBar;
-  }
-  headerBar.itemColor = Color.WHITE;
+
+    /**
+     * @event onShow
+     * This event is called when a page appears on the screen (everytime).
+     */
+    onShow() {
+        super.onShow();
+        this.headerBar.titleLayout.applyLayout();
+        this.routeData && console.info(this.routeData.message);
+    }
+
+    /**
+     * @event onLoad
+     * This event is called once when page is created.
+     */
+    onLoad() {
+        super.onLoad();
+        let headerBar: HeaderBar;
+        this.headerBar.titleLayout = new PageTitleLayout();
+        this.headerBar.setItems([
+            new HeaderBarItem({
+                title: 'Option',
+                onPress: () => {
+                    console.warn('You pressed Option item!');
+                },
+            }),
+        ]);
+        if (System.OS === 'Android') {
+            headerBar = this.headerBar;
+            headerBar.setLeftItem(
+                new HeaderBarItem({
+                    onPress: () => {
+                        this.router.goBack();
+                    },
+                    image: Image.createFromFile('images://arrow_back.png'),
+                }),
+            );
+        } else {
+            headerBar = this.parentController.headerBar;
+        }
+        headerBar.itemColor = Color.WHITE;
+    }
 }
