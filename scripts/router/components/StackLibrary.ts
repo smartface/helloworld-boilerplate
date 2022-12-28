278cd58856df9631caa2bf2a053830cb2987a70a
Suleyman <suleymanbasbug@hotmail.com>
2022-12-28 14:32:41 +0300

    feat: added router on library

---
+++ scripts/router/components/StackLibrary.ts
@@ -0,0 +1,7 @@
+import $$StackLibrary from 'generated/router/components/StackLibrary';
+
+export default class StackLibrary extends $$StackLibrary {
+  constructor(basePath: string, name: string, routeIndex: number) {
+    super(basePath);
+  }
+}
