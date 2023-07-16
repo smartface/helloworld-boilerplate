f6ee67e8dad40f4b217bef9e2425879dc319f195
Guven Karanfil <guven.karanfil@smartface.io>
2023-07-16 17:00:44 +0300

    translation sample

--- App.tsx
+++ App.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 import AppNavigatorUI from './app/routers';
 
 import { ThemeProvider } from './app/context';
+
+// import translation
+import './app/translation';
+
 export default function App() {
   return (
     <ThemeProvider>
--- app/pages/page1/index.tsx
+++ app/pages/page1/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Page1Design, { page1Title, page1HeaderBarClassList } from '@smartface-generated/pages/page1';
+import { useTranslation } from 'react-i18next';
+import i18n from '@app/translation';
+import { Text } from 'react-native';
 
 export const Page1Options = { title: page1Title, headerConfiguration: page1HeaderBarClassList };
 
 export default (props: any) => {
+    const { t } = useTranslation();
     return <Page1Design
+        text1_1={{ label: t('welcome') }}
+        btnLanguage={{
+            onPress: () => {
+                i18n.changeLanguage(i18n.language === 'en' ? 'tr' : 'en')
+            }
+        }}
 
         pressable1={{
             onPress: () => {
