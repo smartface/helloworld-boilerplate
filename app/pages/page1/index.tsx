8eab4e9a3f95565c409ce255715ce63920c87668
Ali Tuğrul Pınar <altgrlpinar@gmail.com>
2023-08-11 12:22:14 +0300

    fix

--- app/pages/page1/index.tsx
+++ app/pages/page1/index.tsx
@@ -32,7 +32,7 @@ export default (props: any) => {
             }}
             pressable1={{
                 onPress: () => {
-                    props.navigation.navigate('modalRouter')
+                    props.navigation.navigate('page2')
                 }
             }}
         />
