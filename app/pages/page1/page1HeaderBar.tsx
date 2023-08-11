8c9f316666829fb30eb6f199975b17ce62302d06
Ali Tuğrul Pınar <altgrlpinar@gmail.com>
2023-08-11 11:31:38 +0300

    fix(pages): headerview & styles

--- app/pages/page1/page1HeaderBar.tsx
+++ app/pages/page1/page1HeaderBar.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { getStyle } from '@smartface-generated/utils';
 
 import { headerBar } from '@smartface-generated/pages/page1';
 
 export const Page1HeaderView = (props: any) => {
-  const insets = useSafeAreaInsets();
 
-  return <headerBar.View headerView={{ style: { height: undefined, width: undefined, paddingTop: insets.top } }} />;
+    return <headerBar.View />;
 };
 
 export default {
-  title: headerBar.title,
-  classList: headerBar.classList,
-  View: Page1HeaderView,
-  hasCustomView: headerBar.hasCustomView
+    title: headerBar.title,
+    classList: headerBar.classList,
+    View: Page1HeaderView,
+    hasCustomView: headerBar.hasCustomView
 };
--- app/pages/page2/page2HeaderBar.tsx
+++ app/pages/page2/page2HeaderBar.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { getStyle } from '@smartface-generated/utils';
 
 import { headerBar } from '@smartface-generated/pages/page2';
 
 export const Page2HeaderView = (props: any) => {
-  const insets = useSafeAreaInsets();
 
-  return <headerBar.View />;
+    return <headerBar.View />;
 };
 
 export default {
-  title: headerBar.title,
-  classList: headerBar.classList,
-  View: Page2HeaderView,
-  hasCustomView: headerBar.hasCustomView
+    title: headerBar.title,
+    classList: headerBar.classList,
+    View: Page2HeaderView,
+    hasCustomView: headerBar.hasCustomView
 };
